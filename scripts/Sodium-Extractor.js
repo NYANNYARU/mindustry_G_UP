ce82af0688943fe23e9150f3eb7233b00d585a44
NYAMINYARU <59269549+NYANNYARU@users.noreply.github.com>
2021-11-26 23:36:43 +0900

    Update Sodium-Extractor.js

--- scripts/Sodium-Extractor.js
+++ scripts/Sodium-Extractor.js
@@ -1,7 +1,17 @@
 const SExtractor = extendContent(GenericCrafter, "Sodium-Extractor", {
+	/* drawBase: function(tile){
+        this.super$drawBase(tile);
+        Draw.color(this.outputLiquid.liquid.color);
+		Draw.alpha(this.liquids.total() / this.liquidCapacity);
+		Draw.rect(Core.atlas.find(this.name + "-liquid"), tile.drawx(), tile.drawy());
+		Draw.color();
+		Draw.rect(Core.atlas.find(this.name + "-top"), tile.drawx(), tile.drawy());
+        Draw.reset();
+	}, */
 });
 
 const water = Liquids.water;
+const Wwater = Liquids.Waste_Water;
 SExtractor.buildType = () => extendContent(GenericCrafter.GenericCrafterBuild, SExtractor, {
 	draw(){
 		Draw.rect(SExtractor.region, this.x, this.y);
@@ -10,14 +20,6 @@ SExtractor.buildType = () => extendContent(GenericCrafter.GenericCrafterBuild, S
 		Draw.rect(Core.atlas.find(SExtractor.name + "-liquid"), this.x, this.y);
 		Draw.color();
 		Draw.rect(Core.atlas.find(SExtractor.name + "-top"), this.x, this.y);
-		Draw.reset();
-	},	
-});
-
-const Wwater = Liquids.Waste_Water;
-SExtractor.buildType = () => extendContent(GenericCrafter.GenericCrafterBuild, SExtractor, {
-	draw(){
-		Draw.rect(SExtractor.region, this.x, this.y);
 		Draw.color(Wwater.color);
 		Draw.alpha(this.liquids.get(Wwater) / SExtractor.liquidCapacity);
 		Draw.rect(Core.atlas.find(SExtractor.name + "-liquid2"), this.x, this.y);
