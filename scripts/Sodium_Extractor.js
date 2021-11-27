642fe5fad4781f81012807d39aab678729131c8f
NYAMINYARU <59269549+NYANNYARU@users.noreply.github.com>
2021-11-27 16:15:04 +0900

    Update Sodium_Extractor.js

--- scripts/Sodium_Extractor.js
+++ scripts/Sodium_Extractor.js
@@ -11,14 +11,15 @@ const NaExtractor = extendContent(GenericCrafter, "Sodium_Extractor", {
 });
 
 const water = Liquids.water;
+const Wwater = "Waste_Water";
 NaExtractor.buildType = () => extendContent(GenericCrafter.GenericCrafterBuild, NaExtractor, {
 	draw(){
 		Draw.rect(NaExtractor.region, this.x, this.y);
 		Draw.color(water.color);
 		Draw.alpha(this.liquids.get(water) / NaExtractor.liquidCapacity);
 		Draw.rect(Core.atlas.find(NaExtractor.name + "-liquid"), this.x, this.y);
-		Draw.color(Liquids.Waste_Water.color);
-		Draw.alpha(this.liquids.get(Liquids.Waste_Water) / NaExtractor.liquidCapacity);
+		Draw.color(WWater.color);
+		Draw.alpha(this.liquids.get(Wwater) / NaExtractor.liquidCapacity);
 		Draw.rect(Core.atlas.find(NaExtractor.name + "-liquid2"), this.x, this.y);
 		Draw.color();
 		Draw.rect(Core.atlas.find(NaExtractor.name + "-top"), this.x, this.y);
