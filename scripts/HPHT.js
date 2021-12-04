d7f82727fb79bd560171ae838117469c0704c94e
NYAMINYARU <59269549+NYANNYARU@users.noreply.github.com>
2021-12-05 01:05:16 +0900

    Update HPHT.js

--- scripts/HPHT.js
+++ scripts/HPHT.js
@@ -22,9 +22,9 @@ hpht.buildType = () => extendContent(GenericSmelter.SmelterBuild, hpht, {
     		Draw.rect(Core.atlas.find(hpht.name + "-heat"), this.x, this.y);
    		Draw.color();
 		Draw.alpha(this.liquids.get(slag) / hpht.liquidCapacity);
-    		Draw.rect(Core.atlas.find(hpht.name + "-heat2"), this.x, this.y);
 		Draw.color();
 		Draw.rect(Core.atlas.find(hpht.name + "-top"), this.x, this.y);
+		Draw.rect(Core.atlas.find(hpht.name + "-heat2"), this.x, this.y);
 		Draw.reset();
 	},
 });
