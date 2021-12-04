3888f24ee4082901a2a852992e5388d00ad69397
NYAMINYARU <59269549+NYANNYARU@users.noreply.github.com>
2021-12-05 00:17:24 +0900

    Create HPHT.js

---
+++ scripts/HPHT.js
@@ -0,0 +1,25 @@
+const hpht = extendContent(GenericSmelter, "HPHT", {
+	/* drawBase: function(tile){
+        this.super$drawBase(tile);
+        Draw.color(this.outputLiquid.liquid.color);
+		Draw.alpha(this.liquids.total() / this.liquidCapacity);
+		Draw.rect(Core.atlas.find(this.name + "-liquid"), tile.drawx(), tile.drawy());
+		Draw.color();
+		Draw.rect(Core.atlas.find(this.name + "-top"), tile.drawx(), tile.drawy());
+        Draw.reset();
+	}, */
+});
+
+const slag = Liquids.slag;
+hpht.buildType = () => extendContent(GenericSmelter.GenericCrafterBuild, hpht, {
+	draw(){
+		Draw.rect(hpht.region, this.x, this.y);
+		Draw.color(slag.color);
+		Draw.alpha(this.liquids.get(slag) / hpht.liquidCapacity);
+		Draw.rect(Core.atlas.find(hpht.name + "-liquid"), this.x, this.y);
+		Draw.color(FF0000);
+    Draw.rect(Core.atlas.find(hpht.name + "-top"), this.x, this.y);
+    Draw.color();
+		Draw.reset();
+	},
+});
