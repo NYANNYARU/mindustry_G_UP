c746098be0e8c244c95c2aac04f398161b6e8230
NYAMINYARU <59269549+NYANNYARU@users.noreply.github.com>
2021-12-09 02:15:33 +0900

    Update Purple_Shadow.js

--- scripts/Purple_Shadow.js
+++ scripts/Purple_Shadow.js
@@ -1,4 +1,8 @@
 const PS = extendContent(ItemTurret, "Purple_Shadow", {
+  this.unitSort = UnitSorts.strongest;
 });
 
-PS.unitSort = UnitSorts.strongest;
+/*PS.unitSort = UnitSorts.strongest;
+PS.buildType = () => extendContent(ItemTurret.ItemTurretBuild, PS, {
+	
+});*/
