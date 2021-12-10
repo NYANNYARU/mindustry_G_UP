227d369da84232d6aff7081e9c1b999168fe2344
NYAMINYARU <59269549+NYANNYARU@users.noreply.github.com>
2021-12-11 00:00:44 +0900

    Update Purple_Shadow.java

--- scripts/Purple_Shadow.java
+++ scripts/Purple_Shadow.java
@@ -1,4 +1,4 @@
-const PS = extendContent(ItemTurret, "Purple_Shadow", {
+public PS = extendContent(ItemTurret, "Purple_Shadow", {
 	icons: function(){
 		return [
 			Core.atlas.find("mindustry_G_UP-Purple_Shadow-base"),
