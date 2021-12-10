0c08c4e06535dddafd4b669fb760e9b40ce8ec36
NYAMINYARU <59269549+NYANNYARU@users.noreply.github.com>
2021-12-11 01:26:55 +0900

    Update Purple_Shadow.js

--- scripts/Purple_Shadow.js
+++ scripts/Purple_Shadow.js
@@ -4,14 +4,11 @@ const extras = require("extras/voidicsm");
 const bombs = require("blocks/bombs")
 
 const PS = extendContent(ItemTurret, "Purple_Shadow", {
+	unitSort: (u, x, y) -> -u.maxHealth + Mathf.dst2(u.x, u.y, x, y) / 6400f;
 	icons: function(){
 		return [
 			Core.atlas.find("mindustry_G_UP-Purple_Shadow-base"),
 			Core.atlas.find("mindustry_G_UP-Purple_Shadow-icon")
 		];
 	}
-});
-
-PS.buildType = () => extendContent(ItemTurret.ItemTurretBuild, PS, {
-	unitSort: (u, x, y) -> -u.maxHealth + Mathf.dst2(u.x, u.y, x, y) / 6400f;
 });
