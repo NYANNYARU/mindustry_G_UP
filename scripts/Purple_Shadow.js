44bff6352004a834058ae4171c63136f5d14f797
NYAMINYARU <59269549+NYANNYARU@users.noreply.github.com>
2021-12-10 23:49:16 +0900

    Update Purple_Shadow.js

--- scripts/Purple_Shadow.js
+++ scripts/Purple_Shadow.js
@@ -2,7 +2,7 @@ const PS = extendContent(ItemTurret, "Purple_Shadow", {
 	icons: function(){
 		return [
 			Core.atlas.find("mindustry_G_UP-Purple_Shadow-base"),
-			Core.atlas.find("mindustry_G_UP-Purple_Shadow")
+			Core.atlas.find("mindustry_G_UP-Purple_Shadow-icon")
 		];
 	}
 });
