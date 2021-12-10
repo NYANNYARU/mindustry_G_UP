effe760cdbc9c59b113c53f6adbf5452b1eb6505
NYAMINYARU <59269549+NYANNYARU@users.noreply.github.com>
2021-12-10 20:32:30 +0900

    Update Purple_Shadow.js

--- scripts/Purple_Shadow.js
+++ scripts/Purple_Shadow.js
@@ -1,6 +1,9 @@
 const PS = extendContent(ItemTurret, "Purple_Shadow", {
   this.unitSort = UnitSorts.strongest;
-  
+	
+	init(){
+        this.super$init();
+    	},
 	icons: function(){
 		return [
 			Core.atlas.find("mindustry_G_UP-Purple_Shadow-base"),
