3884c30e987073564c8e6259c465605ba86cff28
NYAMINYARU <59269549+NYANNYARU@users.noreply.github.com>
2021-12-10 20:44:30 +0900

    Update Purple_Shadow.js

--- scripts/Purple_Shadow.js
+++ scripts/Purple_Shadow.js
@@ -1,12 +1,18 @@
 const PS = extendContent(ItemTurret, "Purple_Shadow", {
-/*	icons: function(){
+	icons: function(){
 		return [
 			Core.atlas.find("mindustry_G_UP-Purple_Shadow-base"),
 			Core.atlas.find("mindustry_G_UP-Purple_Shadow")
 		];
-	} */
+	}
 });
 
 PS.buildType = () => extendContent(ItemTurret.ItemTurretBuild, PS, {
 	this.unitSort = UnitSorts.strongest;
+	icons: function(){
+		return [
+			Core.atlas.find("diamond-ore-godturret-icon-base"),
+			Core.atlas.find("diamond-ore-thor-icon")
+		];
+	}
 });
