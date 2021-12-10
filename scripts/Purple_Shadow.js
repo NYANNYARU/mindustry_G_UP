6e8f921df28107f5fbf51f0ee129412fbe828d6b
NYAMINYARU <59269549+NYANNYARU@users.noreply.github.com>
2021-12-11 01:38:30 +0900

    Update Purple_Shadow.js

--- scripts/Purple_Shadow.js
+++ scripts/Purple_Shadow.js
@@ -1,10 +1,2 @@
-const PS = extendContent(ItemTurret, "Purple_Shadow", {
-	icons: function(){
-		return [
-			Core.atlas.find("mindustry_G_UP-Purple_Shadow-base"),
-			Core.atlas.find("mindustry_G_UP-Purple_Shadow-icon")
-		];
-	}
-});
-
+const PS = extendContent(ItemTurret, "Purple_Shadow", {});
 PS.unitSort = (u, x, y) => -u.maxHealth;
