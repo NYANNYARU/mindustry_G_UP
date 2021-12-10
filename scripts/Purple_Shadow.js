a7aa8415be22a0f976f9ea2bd4faf18f76d58e8b
NYAMINYARU <59269549+NYANNYARU@users.noreply.github.com>
2021-12-10 20:50:09 +0900

    Update Purple_Shadow.js

--- scripts/Purple_Shadow.js
+++ scripts/Purple_Shadow.js
@@ -9,10 +9,4 @@ const PS = extendContent(ItemTurret, "Purple_Shadow", {
 
 PS.buildType = () => extendContent(ItemTurret.ItemTurretBuild, PS, {
 	this.unitSort = UnitSorts.strongest;
-	icons: function(){
-		return [
-			Core.atlas.find("diamond-ore-godturret-icon-base"),
-			Core.atlas.find("diamond-ore-thor-icon")
-		];
-	}
 });
