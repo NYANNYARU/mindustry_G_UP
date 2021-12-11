da56ebb5efef778e8ca56dba45c1d520a5b526fa
NYAMINYARU <59269549+NYANNYARU@users.noreply.github.com>
2021-12-11 20:58:05 +0900

    Create effects.js

---
+++ scripts/effects.js
@@ -0,0 +1,17 @@
+const color1 = Color.valueOf("9a79f7");
+const instShoot_P = newEffect(60, e => {
+    e.scaled(10, b -> {
+            color(Color.white, color1, b.fin());
+            stroke(b.fout() * 3f + 0.2f);
+            Lines.circle(b.x, b.y, b.fin() * 50f);
+        });
+
+        color(color1);
+
+        for(int i : Math.signs){
+            Draw.tri(e.x, e.y, 13 * e.fout(), 85, e.rotation + 90 * i);
+            Draw.tri(e.x, e.y, 13 * e.fout(), 50, e.rotation + 20 * i);
+        }
+
+        Draw.light(e.x, e.y, 180, color1, 0.9 * e.fout());
+});
